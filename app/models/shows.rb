f2e46cfe9e519e7f5f83637881ea10efab7d7c32
Landon Reasoner <reasoner.landon90@gmail.com>
2021-04-11 19:45:42 +0000

    Automatically backed up by Learn

--- app/models/shows.rb
+++ app/models/shows.rb
@@ -19,4 +19,12 @@ class Show < ActiveRecord::Base
   def Show::ratings_sum
     Show.sum('rating')
   end
+  
+  def Shows::popular_shows
+    Show.where('rating > 5')
+  end
+  
+  def Show::shows_by_alphabetical_order
+    Show.order(name: :asc)
+  end
 end
